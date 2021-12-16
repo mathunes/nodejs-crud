f37c7570226e63da4838ad185bada3a284c419a3
mathunes <matheusantunes720@gmail.com>
2021-12-16 15:53:49 -0300

    add use case List Categories

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
 
 const routes = Router();
 
 //handle automatically receive request and response
 routes.post("/categories", new CreateCategoryController().handle);
+
+routes.get("/categories", new GetAllCategoriesController().handle);
 
 export { routes };
