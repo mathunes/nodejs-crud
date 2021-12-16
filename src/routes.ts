780c318a9afe76ad8b61419da305c8d2069ac972
mathunes <matheusantunes720@gmail.com>
2021-12-16 16:01:59 -0300

    add use case Delete Category

--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateCategoryController } from "./controllers/CreateCategoryController";
+import { DeleteCategoryController } from "./controllers/DeleteCategoryController";
 import { GetAllCategoriesController } from "./controllers/GetAllCategoriesController";
 
 const routes = Router();
@@ -8,5 +9,7 @@ const routes = Router();
 routes.post("/categories", new CreateCategoryController().handle);
 
 routes.get("/categories", new GetAllCategoriesController().handle);
+
+routes.delete("/categories/:id", new DeleteCategoryController().handle);
 
 export { routes };
